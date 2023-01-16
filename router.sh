444ffaf0eab8b794196ffee3f270ee6fb4e26253
Adam Olech <me@aole.ch>
2023-01-16 23:49:23 +0100

    router.sh: use export_config.py to get conf variables

--- router.sh
+++ router.sh
@@ -1,20 +1,18 @@
-#!/bin/sh
+#!/usr/bin/env bash
 set -e
 
-WAN=wlp0s20f3
-#LAN=enp58s0u1u4u3
-LAN=enp0s20f0u4
-GW="10.3.189.1/24"
-DHCP_MAX_LEASES=90
-DHCP_LEASE_OFFSET=10
-IPS_IN_SUBNET=$(nmap -sL -n $GW \
+this_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+eval "$($this_dir/utils/export_config.py $1)"
+
+IPS_IN_SUBNET=$(nmap -sL -n $interfaces__gateway \
     | awk '/Nmap scan report/{print $NF}' \
     | head -n -1 \
     | tail -n +2
 )
 IPS_FOR_DHCP=$(echo "$IPS_IN_SUBNET" \
-    | head -n +${DHCP_MAX_LEASES} \
-    | tail -n +${DHCP_LEASE_OFFSET} \
+    | head -n +${dhcp__max_leases} \
+    | tail -n +${dhcp__lease_offset} \
     | sed -e 1b -e '$!d' \
     | tr '\n' ',' \
     | sed 's/,$/\n/'
@@ -22,7 +20,7 @@ IPS_FOR_DHCP=$(echo "$IPS_IN_SUBNET" \
 C='-m comment --comment "simple-router"'
 
 _term() {
-    ip addr del $GW dev $LAN
+    ip addr del $interfaces__gateway dev $interfaces__lan
 	iptables-save | grep -v "simple-router" | iptables-restore
 	exit 0
 }
@@ -33,18 +31,18 @@ trap _term EXIT
 
 set -x
 
-ip addr add $GW dev $LAN
+ip addr add $interfaces__gateway dev $interfaces__lan
 
 echo 1 > /proc/sys/net/ipv4/ip_forward
 
-iptables -t nat -A POSTROUTING -o $WAN -j MASQUERADE $C
-iptables -I FORWARD 1 -i $LAN -j ACCEPT $C
-iptables -I FORWARD 1 -o $LAN -m state --state RELATED,ESTABLISHED -j ACCEPT $C
+iptables -t nat -A POSTROUTING -o $interfaces__wan -j MASQUERADE $C
+iptables -I FORWARD 1 -i $interfaces__lan -j ACCEPT $C
+iptables -I FORWARD 1 -o $interfaces__lan -m state --state RELATED,ESTABLISHED -j ACCEPT $C
 
 /usr/sbin/dnsmasq \
-    --interface=$LAN \
+    --interface=$interfaces__lan \
     --bind-interfaces \
-    --dhcp-range=$IPS_FOR_DHCP,12h \
+    --dhcp-range=$IPS_FOR_DHCP,2m \
     --log-dhcp \
     --leasefile-ro \
     --dhcp-authoritative \
